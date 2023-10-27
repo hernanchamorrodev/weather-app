0951aa9fa4d6702a5341af4e8fd706bfc5c78142
Hernan CH <hchamorrodev@gmail.com>
2023-10-27 06:15:50 -0300

    tailwind-init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex justify-center items-center bg-gradient-to-br from-slate-100 via-sky-300 to-sky-400 h-[100vh] w-full">
+      Weather app
+    </main>
   );
 }
 
