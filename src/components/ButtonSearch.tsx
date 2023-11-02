49eb17e83cd42358d5187ce29c244d95b1c5e2b8
Hernan CH <hchamorrodev@gmail.com>
2023-11-02 10:28:29 -0300

    button-search

--- src/components/ButtonSearch.tsx
+++ src/components/ButtonSearch.tsx
@@ -1,6 +1,6 @@
 const ButtonSearch = () => {
   return (
-    <button className="bg-blue-950 text-white rounded-md w-1/4 my-3">
+    <button className="hover:bg-transparent hover:border-blue-950 bg-blue-950 hover:border hover:text-blue-950 hover:font-semibold text-white rounded-md w-1/4 my-3">
       Search
     </button>
   );
